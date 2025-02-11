f06dc6489d42121685f2da23590d9421e1b340ab
Wilmar Diaz Soto <wilmardiaz637@gmail.com>
2025-02-11 15:04:16 -0500

    Inicialización del Proyecto

---
+++ index.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<!--
+To change this license header, choose License Headers in Project Properties.
+To change this template file, choose Tools | Templates
+and open the template in the editor.
+-->
+<html>
+    <head>
+        <meta charset="UTF-8">
+        <title></title>
+    </head>
+    <body>
+        <?php
+        /* Generalmente en el index no se programa entonces utilizo esta linea de codigo
+        para enlacer el formulario o la pagina web donde voy a programar */
+        echo "<script languaje='Javascript'>document.location.href= 'index.html';</script>";
+        ?>
+    </body>
+</html>
